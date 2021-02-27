ea113f3666ce09ee72e62b980569670041323a9f
Jake Casto <jake@lynndigital.com>
2021-02-27 14:23:06 -0500

    Improve template with sentry logging

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
+import Toucan from "toucan-js"
+
 addEventListener('fetch', event => {
-  	event.respondWith(handleRequest(event.request))
+	const sentry = new Toucan({
+		dsn: "...",
+		event,
+		allowedHeaders: ["user-agent"],
+		allowedSearchParams: /(.*)/,
+	});
+
+	event.respondWith(handleRequest(event, sentry))
 })
+
 /**
- * Respond with hello worker text
- * @param {Request} request
+ * Respond to the request
+ * @param {Event} event
+ * @param {Toucan} sentry
  */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+async function handleRequest(event, sentry) {
+	/** @param {Request} request */
+	const request = event.request
+	
+	try {
+		return new Response('Hello worker!', {
+			headers: { 'content-type': 'text/plain' },
+		})
+	}
+	catch(e) {
+		sentry.captureException(e)
+
+		return new Response('Worker threw exception', {
+			headers: { 'content-type': 'text/plain' },
+			status: 500
+		})
+	}
 }
